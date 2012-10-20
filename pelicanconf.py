54781acb11b12be29aac44b32af2987580d1caec
jawher <jawher.moussa@gmail.com>
2012-10-20 05:33:02 +0200

    Tweak site conf (author name and mail and site name)

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = u"Jawher"
-SITENAME = u"Jawher"
+AUTHOR = u"Jawher Moussa"
+AUTHOR_EMAIL = u"firstName.lastName@gmail.com"
+SITENAME = u"Jawher's characters depot"
 SITEURL = 'http://jawher.me'
 
 TIMEZONE = 'Europe/Paris'
